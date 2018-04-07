b8870f002e8badeada0475e8100c5af8ab98b25e
suniljackie <31463813+suniljackie@users.noreply.github.com>
2018-04-07 18:10:28 +0530

    google search added

---
+++ src/package1/GoogleSearch.java
@@ -0,0 +1,36 @@
+package package1;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GoogleSearch {
+
+	WebDriver d;
+
+	@Test
+	public void googlesearch() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "D:\\MS OFFICE\\SELENIUM\\chromedriver.exe");
+		d = new ChromeDriver();
+		d.get("https://www.google.co.in");
+		d.manage().window().maximize();
+		Thread.sleep(2000);
+		d.findElement(By.id("lst-ib")).sendKeys("selenium");
+		Thread.sleep(3000);
+		List<WebElement> drop = d.findElements(By.xpath(".//ul[@role='listbox']/li/descendant::div[@class='sbqs_c']"));
+		System.out.println(drop.size());
+
+		for (int i = 0; i < drop.size(); i++) {
+			String links = drop.get(i).getText();
+			if (drop.get(i).getText().contains("selenium framework")) {
+				drop.get(i).click();
+				System.out.println("succesfully Clicked");
+			}
+			System.out.println(links);
+		}
+	}
+}
